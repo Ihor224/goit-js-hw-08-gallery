32bf1785052076cd35a0d3d6807a8fc566cf0b54
Ihor Levchenko <ihor224@gmail.com>
2020-10-17 20:40:37 +0300

    Открывает и закрывает модалку

---
+++ js/gallery.js
@@ -0,0 +1,67 @@
+import galleryItems from "./gallery-items.js";
+
+const refs = {
+  galleryList: document.querySelector(".js-gallery"),
+  overlayEl: document.querySelector(".lightbox__overlay"),
+  modalEl: document.querySelector(".js-lightbox"),
+  modalImgEl: document.querySelector(".lightbox__image"),
+  closeBtnEl: document.querySelector('button[data-action="close-lightbox"]'),
+};
+
+const cardsMarkup = createGalleryItems(galleryItems);
+refs.galleryList.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryItems(e) {
+  return e
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+refs.galleryList.addEventListener("click", modalOpenClick);
+
+function modalOpenClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  document.addEventListener("keydown", closeModalByEscape);
+
+  refs.modalEl.classList.add("is-open");
+  refs.modalImgEl.src = e.target.dataset.source;
+  refs.modalImgEl.alt = e.target.alt;
+}
+
+refs.closeBtnEl.addEventListener("click", closeModal);
+
+function closeModal() {
+  refs.modalEl.classList.remove("is-open");
+  refs.modalImgEl.src = "";
+}
+
+function closeModalByOverlay() {
+  refs.overlayEl.addEventListener("click", closeModal);
+}
+
+refs.overlayEl.addEventListener("click", closeModalByOverlay);
+
+function closeModalByEscape(e) {
+  if (e.code === "Escape") {
+    closeModal();
+  }
+}
